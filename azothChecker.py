4547befe627ca85fc09ce73217661ca1afb75a50
Lxghtend <140092221+Lxghtend@users.noreply.github.com>
2023-10-19 13:39:00 -0400

    Add files via upload

--- azothChecker.py
+++ azothChecker.py
@@ -2,7 +2,6 @@ import os
 import subprocess
 import wizwalker
 import asyncio
-import pyautogui
 from wizwalker import Client, client
 from wizwalker.constants import Keycode
 from wizwalker.memory import Window
@@ -12,6 +11,11 @@ from wizwalker import ClientHandler
 spellbook = ["WorldView", "DeckConfiguration"]
 playButton = ['WorldView', 'mainWindow', 'btnPlay']
 quitButton = ['WorldView', 'DeckConfiguration', 'SettingPage', 'QuitButton']
+tcButton = ["WorldView", "DeckConfiguration", "DeckConfigurationWindow", "ControlSprite", "TreasureCardButton"]
+
+total_azoth_stacks = 0
+remainder_azoth = 0
+
 
 
 async def is_visible_by_path(base_window:Window, path: list[str]):
@@ -19,6 +23,8 @@ async def is_visible_by_path(base_window:Window, path: list[str]):
     if window := await window_from_path(base_window, path):
         return await window.is_visible()
     return False
+
+
 
 # Returns a window, given a path 
 async def window_from_path(base_window:Window, path:list[str]) -> Window:
@@ -32,11 +38,14 @@ async def window_from_path(base_window:Window, path:list[str]) -> Window:
     return False
 
 
+
 async def click_window_from_path(mouse_handler, base_window, path): #credit ultimate314
     try:
         await mouse_handler.click_window(await window_from_path(base_window, path))
     except:
         pass
+
+
 
 async def click_window_until_gone(client, path): #i did this im very cool - milwr (ithink)
     while (window := await window_from_path(client.root_window, path)) and await is_visible_by_path(client.root_window, path):
@@ -45,105 +54,153 @@ async def click_window_until_gone(client, path): #i did this im very cool - milw
 
 
 
+async def get_player_school(p) -> str: # Made by notfaj, edited by starrfox
 
-
-async def get_player_school(p) -> str: # FAJ MADE THIS ONE TOO, starr fixed it
     txtLevel = ['WorldView', 'mainWindow', 'sprSubBanner', 'txtLevel']
+
     window : Window = await window_from_path(p.root_window, txtLevel)
+
+    if window:
+      return await window.maybe_text()
+    return ""
+
+
+    
+async def check_tc(p):
+
+    tc = [ "WorldView", "DeckConfiguration", "DeckConfigurationWindow", "ControlSprite", "DeckPage", "TreasureCardCount" ]
+
+    window : Window = await window_from_path(p.root_window, tc)
+
     if window:
       return await window.maybe_text()
     return ""
 
 
 
-
-async def tc():
-    center_location = pyautogui.locateCenterOnScreen("tc.png")
+async def crownshop_visibility(p): # Returns if crownshop is visible
     
-    return center_location
+    try: 
+        if await ((await p.root_window.get_windows_with_name('permanentShop'))[0]).is_visible():
+            return True
+        
+    except: 
+        return False
 
 
-async def tc_button():
-    tc_bttn = pyautogui.locateCenterOnScreen("tcbutton.png") # i dont know how to click sprites
-    
-    pyautogui.click(tc_bttn)
-    
-    await asyncio.sleep(0.1)
-    
-total_azoth_stacks = 0
 
-async def crown_shop():
-    crownshop = pyautogui.locateCenterOnScreen("crownshop.png")
+async def main_checker(p, remainder_azoth, total_azoth_stacks):
 
-    if crownshop != '':
-        pyautogui.click(crown_shop)
-
-async def main_checker(p, total_azoth_stacks):
-    await p.mouse_handler.deactivate_mouseless()
-    await asyncio.sleep(5)
-    #await crown_shop()
-
-    while not await is_visible_by_path(p.root_window, spellbook):
-        await p.send_key(Keycode.P)
+    # Close crown shop if its visible
+    while await crownshop_visibility(p) == True:
+        await p.send_key(Keycode.ESC, 0.1)
         await asyncio.sleep(0.1)
 
+    # Navigate to tc window
+    #while not is_visible_by_path(p.root_window, tcButton):
+    await p.send_key(Keycode.P)
+    await asyncio.sleep(0.1)
     await asyncio.sleep(1)
-    await tc_button()
-    await asyncio.sleep(0.5)
-    if await tc() != "":
+    await click_window_from_path(p.mouse_handler, p.root_window, tcButton)
+
+    # Create a variable for TC
+    tc = await check_tc(p)
+    tc = tc.replace("<center>", '').replace("</center>", '').replace("/999", '')
+    tc = int(tc)
+
+    # Add to the variable if stacks are found
+    if tc == 999:
         total_azoth_stacks = total_azoth_stacks + 1
-        print(total_azoth_stacks)
-    else:
-        pass
-    await p.mouse_handler.activate_mouseless()
-    while not await is_visible_by_path(p.root_window, quitButton):
-        await p.send_key(Keycode.ESC, 0.05)
-    if await is_visible_by_path(p.root_window, quitButton):
-        await click_window_from_path(p.mouse_handler, p.root_window, quitButton)
+        print("Total Azoth (in stacks):", total_azoth_stacks)
+
+    if tc << 999:
+        remainder_azoth = remainder_azoth + tc
+        print("Azoth Not In Full Stacks:", remainder_azoth)
+
+    if tc == 0:
+        print("No azoth found on current wizard.")
     
+    # Quit out to prepare for the next wizard
+    while not await is_visible_by_path(p.root_window, quitButton):
+        await p.send_key(Keycode.ESC, 0.1)
+
+    await click_window_until_gone(p, quitButton)
+                
 
 
-async def logic(p):
+async def new_logic(p):
+
+    # Attaches hooks
     await setup(p)
-    wizards = [] # create list for all wizards
-    wizard = await get_player_school(p) # current wizard on the screen school is captured
-    print(await get_player_school(p))
-    wizard = wizard.split(' ')
-    #wizard[3] = wizard[3].replace(')', '') # changes ex. level 160 (prime diviner), to 160 diviner
-    length = len(wizards)
-    while wizard != "" and length != 6: # when the school isnt blank, idk why i added this tbh..., this should add all 6 wizards to the list then stop, if u have 7 on a storage acc for some rzn, change it to 7
-        if wizard not in wizards: # if the current wizard is not already in the list
-            wizards.append(wizard)    # add it
-            await p.send_key(Keycode.TAB, 0.1) # go to next wizard
-    print("Checking for azoth stacks on", wizards)
+
+    # Defining lists that are used later
+    wizards = []
+    wizards_print = []
+
+    # Add wizards to a list
+    for i in range(6): # Repeats 6 times, if you have 7 characters for some reason, DM me on discord and ill make you a seperate version (@Lxghtend)
+
+        wizard = await get_player_school(p)
+        
+        # This block simply formats text that is later displayed in the terminal (decorative)
+        wizard_print = wizard.replace('<center>', '').replace('</center>', '')
+        wizard_print = wizard_print.split(' ')
+        wizard_print[3] = wizard_print[3].replace(')', '') # Changes ex. Level 160 (Prime Diviner), to 160 Diviner
+        wizard_print = wizard_print[1], wizard_print[3]
+        wizard_print = ' '.join(wizard_print)
+        wizards_print.append(wizard_print)
+
+        # Adds wizard to list wizards
+        wizards.append(wizard)
+
+        # Switch to the next wizard
+        await p.send_key(Keycode.TAB)
+        await asyncio.sleep(0.2)
+
+    print("Azoth Checker is checking wizards:", wizards_print)
+
     time = 0
-    for i in range(length):
+
+    # Runs the checker
+    for i in range(6): # Repeats 6 times, if you have 7 characters for some reason, DM me on discord and ill make you a seperate version (@Lxghtend)
+
+        wizard = await get_player_school(p)
+
         while wizard != wizards[time]:
             await p.send_key(Keycode.TAB, 0.1)
+            wizard = await get_player_school(p)
+
         if wizard == wizards[time]:
-            await p.click_window_until_gone(p.mouse_handler, p.root_window, playButton)
+            await click_window_until_gone(p, playButton)
             await asyncio.sleep(8)
-            await main_checker(p)
+            await main_checker(p, remainder_azoth, total_azoth_stacks)
             time = time + 1
-    subprocess.call(f"taskkill /F /PID {p.process_id}",stdout=subprocess.DEVNULL) #kills the current wizard client
 
-            
+    subprocess.call(f"taskkill /F /PID {p.process_id}",stdout=subprocess.DEVNULL) # Kills the current wizard client
 
-async def setup(p): #activates all hooks that it can for a client
+
+
+async def setup(p): #Activates needed hooks for a client
+
     print("Activating Special Lxghtend Hooks :o :p :3")
+
     await p.hook_handler.activate_root_window_hook(wait_for_ready=False)
     await p.hook_handler.activate_render_context_hook(wait_for_ready=False)
     await p.mouse_handler.activate_mouseless()
 
 
+
 async def startup():
+
     print("""
     Azoth Checker By Lxghtend      
     Credits:
     Milwr - used A LOT of his code in this bot
-    Notfaj - helpin as always
-    Major - helped with a silly little error that should have been obvious
+    Notfaj - helping as always
+    Major - helped fix my bad code
               """) 
+    
+    # This block starts, logs in, and defines a client but it confuses me so
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),"accounts.txt")) as my_file:
             accountList = [
                 line.strip().split(":") for line in my_file.read().split("\n") #reads account list and puts into into a list
@@ -157,46 +214,8 @@ async def startup():
                 p = Client(handle) #defines a client
                 await asyncio.sleep(5)
                 await p.send_key(Keycode.P)
-                await asyncio.sleep(10)
+                await asyncio.sleep(2)
                 await new_logic(p) 
-
-
-
-
-                
-
-async def new_logic(p):
-    await setup(p)
-    wizards = []
-    wizards_print = []
-    for i in range(6):
-        wizard = await get_player_school(p)
-        wizard_print = wizard.replace('<center>', '').replace('</center>', '')
-        wizard_print = wizard_print.split(' ')
-        wizard_print[3] = wizard_print[3].replace(')', '') # changes ex. level 160 (prime diviner), to 160 diviner
-        wizard_print = wizard_print[1], wizard_print[3]
-        wizard_print = ' '.join(wizard_print)
-        wizards_print.append(wizard_print)
-        wizards.append(wizard)
-        await p.send_key(Keycode.TAB)
-        await asyncio.sleep(1)
-    print("Azoth Checker is using wizards:", wizards_print)
-    print(wizards[0])
-    time = 0
-    for i in range(6):
-        wizard = await get_player_school(p)
-        while wizard != wizards[time]:
-            await p.send_key(Keycode.TAB, 0.1)
-            wizard = await get_player_school(p)
-        if wizard == wizards[time]:
-            await click_window_until_gone(p, playButton)
-            await asyncio.sleep(8)
-            await main_checker(p, total_azoth_stacks)
-            time = time + 1
-    subprocess.call(f"taskkill /F /PID {p.process_id}",stdout=subprocess.DEVNULL) #kills the current wizard client
-
-
-
 
 
 
